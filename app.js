a9f8707e468487ae8a009d41f371a20a265a054a
Renato Martins <guillenmartins@gmail.com>
2023-03-14 21:08:30 +0000

    docker compose update

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 //jshint esversion:6
 const express=require("express");
 const app=express();
-const MongoClient = require('mongodb').MongoClient;
-
-//Body Parser
+const mongoose=require("mongoose");
+const { Schema } = mongoose;
 const bodyParser=require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
-
 const { v4: uuidv4 } = require('uuid');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 //ejs
 let ejs = require('ejs');
 app.set("view engine","ejs")
-
 app.use(express.static(__dirname));
 const server_port= process.env.PORT || 3000;
 
@@ -20,20 +19,25 @@ const server_port= process.env.PORT || 3000;
 let posts=[];
 
 /* Mongodb*/
-const url='mongodb://root:example@mongo:27017/?maxPoolSize=20&w=majority';
-const client=new MongoClient(url,{monitorCommands:true});
-client.on('commandStarted', started => console.log(started));
+const url='mongodb://root:example@mongo:27017/blogs-db';
+
+//schema
+const blogSchema=new Schema({
+title:String,
+message:String,
+id:String
+});
+
+//model
+const Blog=mongoose.model("Blog",blogSchema);
 
 app.listen(server_port,function(){
     console.log("server running on port "+server_port);
 });
 
-app.get("/",(req,res)=>{
+app.get("/", async(req,res)=>{
     
-    readFromMongo().then(data=>{
-        res.render("index",{posts:data});
-    });
-    //res.render("index",{posts:posts_from_db});
+    res.render("index",{posts:posts});
 });
 
 app.get("/about",(req,res)=>{
@@ -48,47 +52,40 @@ app.get("/blogger",(req,res)=>{
     res.render("blogger");
 });
 
-app.post("/blogger",(req,res)=>{
+app.post("/blogger", (req,res)=>{
 
-    var post={
-        id:uuidv4(),
-        title:req.body.blogPostTitle,
-        message:req.body.blogPostMessage
-    };
-    posts.push(post);
-   saveToMongoDb(post);
-    res.redirect("/");
+   
+   let blog=new Blog({
+    id:uuidv4(),
+    title:req.body.blogPostTitle,
+    message:req.body.blogPostMessage
+   });
+    
+    saveToMongo(blog).then(()=>res.redirect("/")).catch(()=>res.redirect("/contact"));
+   
 
 });
 
 app.get("/post/:blogId",(req,res)=>{
 
-    var blog_id=req.params.blogId;
-    var found_post=posts.find(post=>post.id===blog_id);
+    let blog_id=req.params.blogId;
+    let found_post=posts.find(post=>post.id===blog_id);
     res.render("blog",{post:found_post});
 });
 
-async function saveToMongoDb(post){
-    console.log("Saving to mongo");
-
-    const database = client.db("blog-posts");
-    const collection = database.collection("blogs");
-    const result = await collection.insertOne(post);
-    console.log(
-       `A document was inserted with the _id: ${result}`,
-    );
-}
-
-async function readFromMongo(){
-
-    console.log("reading from Mongo");
-    var results=[];
-    const database = client.db("blog-posts");
-    const collection = database.collection("blogs");
-    var cursor=await collection.find({});
-    var docs=await cursor.toArray();
-    for await (const doc of docs) {
-        results.push(doc);
-      }
-    return results;
+async function saveToMongo(post) {
+    console.log("About to save");
+    await mongoose.connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        family:4
+      }).then(()=>{
+          console.log("Connected to Mongo with Mongoose");
+      }).catch((err) => { 
+          console.log("Error connecting to mongodb with mongoose " + err);
+          console.error(err);
+      });
+    
+    
+    post.save();
 }
